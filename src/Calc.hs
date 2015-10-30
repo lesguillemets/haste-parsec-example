bd126e4ee8259d6334000c0680309e126523d256
lesguillemets <lesguillemets@users.noreply.github.com>
2015-10-31 02:41:55 +0900

    example

--- src/Calc.hs
+++ src/Calc.hs
@@ -1,4 +1,16 @@
 import Haste
+import Haste.DOM
+import Haste.Events
 import Text.Parsec
 
-main = undefined
+main = do
+    Just input <- elemById "calcInput"
+    Just output <- elemById "output"
+    onEvent input KeyUp $ \ e -> do
+        Just l <- getValue input
+        case parse p "" l of
+             Right s -> setProp output "innerText" (unlines . map reverse $ s)
+             Left _ -> return ()
+
+p :: Parsec String u [String]
+p = (many $ noneOf ",.!") `sepEndBy` (oneOf ",.!")
